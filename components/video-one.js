6eccd359b69efe71100445de6f3b893f746bda6a
sanjay jawahar <balasanjaykumar@gmail.com>
2022-06-11 22:15:06 +0530

    init commit

---
+++ components/video-one.js
@@ -0,0 +1,44 @@
+import React, { Fragment, useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import SectionTitle from "@/components/section-title";
+import { VideoOneData } from "@/data";
+import "react-modal-video/css/modal-video.min.css";
+
+const VideoOne = () => {
+  const [isOpen, setOpen] = useState(false);
+  const { sectionContent, video } = VideoOneData;
+  return (
+    <Fragment>
+      <section className="commonSection our_work">
+        <div className="videoWrap_2">
+          <img src={video.image} alt={video.title} />
+          <div className="play_video">
+            <a
+              onClick={(e) => {
+                e.preventDefault();
+                setOpen(true);
+              }}
+              href="#"
+              className="video_popup"
+            >
+              <i className="fa fa-play"></i>
+            </a>
+            <h2>{video.title}</h2>
+          </div>
+        </div>
+        <Container>
+          <Row>
+            <Col lg={6} sm={12}></Col>
+            <Col lg={6} sm={12}>
+              <div className="ab_detail_wrap">
+                <SectionTitle data={sectionContent} />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Fragment>
+  );
+};
+
+export default VideoOne;
